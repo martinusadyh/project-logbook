f26a1a0faa75defc3ae52437351a5cd28823714b
Martinus Ady H <martinus@artivisi.com>
2011-10-02 22:29:27 +0700

    Project setup of LogBook Application

---
+++ impl/src/main/java/id/web/martinusadyh/logbook/service/impl/App.java
@@ -0,0 +1,13 @@
+package id.web.martinusadyh.logbook.service.impl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ jpa/src/main/java/id/web/martinusadyh/logbook/jpa/App.java
@@ -0,0 +1,13 @@
+package id.web.martinusadyh.logbook.jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
